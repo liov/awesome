9e5ecb55b265c6c7c2f1fdf571650fffd3361359
贾一饼 <lby.i@qq.com>
2024-07-18 08:00:01 +0800

    update

--- lang/shell/git.sh
+++ lang/shell/git.sh
@@ -1,33 +1,31 @@
-#!/bin/bash
+function update_commit_time_if_needed() {
+  # 获取最后一次提交的哈希值
+  last_commit_hash=$(git log -1 --format=%H)
 
-cd $1
+  # 获取最后一次提交的时间
+  last_commit_time=$(git show -s --format=%ci $last_commit_hash) # ci提交时间 ai 作者时间
+  echo "最后一次提交时间: $last_commit_time"
+  # 转换时间格式
+  commit_time=$(date -d "$last_commit_time" +"%Y-%m-%d %H:%M:%S")
+  current_time=$(date +"%Y-%m-%d %H:%M:%S")
 
-# 获取最后一次提交的哈希值
-last_commit_hash=$(git log -1 --format=%H)
+  # 获取星期几，1-5 表示周一到周五，0和6表示周日和周六
+  weekday=$(date -d "$commit_time" +%u)
 
-# 获取最后一次提交的时间
-last_commit_time=$(git show -s --format=%ct $last_commit_hash)
+  # 获取小时数
+  hour=$(date -d "$commit_time" +%H)
 
-# 将时间戳转换为日期时间格式（这里使用UTC时间）
-formatted_last_commit_time=$(date -u -d @$last_commit_time +"%Y-%m-%d %H:%M:%S")
+  # 检查是否在周一到周五的9点到19点之间
+  if [[ $weekday -ge 1 && $weekday -le 5 && $hour -ge 9 && $hour -lt 19 ]]; then
+      # 计算当前时间前十个小时的时间
+      new_time=$(date -d '-10 hours' '+%Y-%m-%d %H:%M:%S')
 
-echo "Last commit was at: $formatted_last_commit_time UTC"
+      # 修改最后一次提交的时间
+      GIT_AUTHOR_DATE="$new_time" GIT_COMMITTER_DATE="$new_time" git commit --amend --no-edit --date "$new_time"
+      echo "提交时间已修改为: $new_time"
+  else
+      echo "最后一次提交时间不在周一到周五的9-19点之间，无需修改。"
+  fi
+}
 
-# 计算上次提交是否在周一至周五的9:30到18:30之间
-day_of_week=$(( ($(date -d @$last_commit_time +%u) % 7) ))
-hour=$(date -d @$last_commit_time +%H)
-
-if [[ $day_of_week -ge 1 && $day_of_week -le 5 ]] && [[ $hour -ge 9 && $hour -lt 18 ]]; then
-    echo "The last commit was within the target time range."
-
-    # 如果满足条件，将时间戳减去10小时
-    adjusted_time="$(date -d '-10 hours' '+%Y-%m-%d %H:%M:%S')"
-
-    # 注意：下面的步骤不建议在真实环境中执行，因为这会修改Git历史
-    # 若要演示，可以打印出将要执行的命令而非真正执行
-    echo "Would run: git commit --amend --date=$adjusted_time --no-edit"
-    # 实际执行命令应替换上一行注释的命令如下
-    git commit --amend --date=$adjusted_time --no-edit
-else
-    echo "The last commit was outside the target time range."
-fi
+update_commit_time_if_needed
