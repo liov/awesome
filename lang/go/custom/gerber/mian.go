81318275df7526b857c19992cbbbaee08105d4ec
贾一饼 <lby.i@qq.com>
2024-08-12 18:44:17 +0800

    gerber

--- lang/go/custom/gerber/mian.go
+++ lang/go/custom/gerber/mian.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/hopeio/utils/encoding/gerber"
 	"github.com/hopeio/utils/log"
 	"os"
-	"test/custom/gerber/parse"
+
 	"test/custom/gerber/svg"
 )
 
 func main() {
 	log.Println("开始")
 	p := svg.NewProcessor()
-	processor := parse.NewParser(p)
+	processor := gerber.NewParser(p)
 	file, err := os.OpenFile(`D:\work\Gerber\Gerber_TopLayer.GTL`, os.O_RDONLY, 0)
 	if err != nil {
 		log.Fatal(err)
--- lang/python/README.md
+++ lang/python/README.md
@@ -4,3 +4,6 @@
 依赖会找mscv去编译,比如matplotlib
 https://github.com/msys2/MINGW-packages/issues/18192
 https://www.msys2.org/docs/python/#known-issues
+当然你可以使用pacman -S mingw-w64-ucrt-x86_64-python-matplotlib
+
+只是所有涉及编译的依赖都要这样，会很麻烦
