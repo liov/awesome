d7dfd92b8d616c744f3790f168129bba388b3626
贾一饼 <lby.i@qq.com>
2024-08-30 23:33:47 +0800

    avx2

--- lang/go/custom/asm/asm/wrap.go
+++ lang/go/custom/asm/asm/wrap.go
@@ -1,9 +1,13 @@
 package asm
 
-import "unsafe"
+import (
+	"fmt"
+	"unsafe"
+)
 
-func Avx2_ssd_int16(a, b []int16, l int) int32 {
-	var result int32
-	avx2_ssd_int16(unsafe.Pointer(&a[0]), unsafe.Pointer(&b[0]), unsafe.Pointer(&result), unsafe.Pointer(uintptr(len(a))))
-	return result
+func Avx2_ssd_int16(a, b []int16) int32 {
+	sum := make([]int32, 8)
+	avx2_ssd_int16(unsafe.Pointer(&a[0]), unsafe.Pointer(&b[0]), unsafe.Pointer(&sum[0]))
+	fmt.Println(a, b, sum)
+	return sum[4] + sum[5] + sum[6] + sum[7]
 }
---
+++ lang/go/custom/asm/avx_asm.go
@@ -0,0 +1,50 @@
+package main
+
+/*
+#cgo CFLAGS: -mavx2 -std=c11
+
+#include <immintrin.h>
+#include <stdint.h>
+#include <stddef.h>
+
+void avx2_ssd_int16(const int16_t *a, const int16_t *b,const int32_t *sums) {
+
+    __m256i sum = _mm256_setzero_si256(); // 初始化累加器
+    __m256i va = _mm256_loadu_si256((const __m256i*)a); // 加载 16 个 int16
+    __m256i vb = _mm256_loadu_si256((const __m256i*)b); // 加载 16 个 int16
+    __m256i vdiff = _mm256_sub_epi16(va, vb); // 计算差值
+
+     // 计算平方
+     __m256i vsquare = _mm256_madd_epi16(vdiff, vdiff); // 计算差值的平方
+
+    // 使用 _mm256_add_epi64 累加差的平方和
+    _mm256_store_si256((__m256i*)&sums, vsquare);
+
+}
+
+*/
+import "C"
+import (
+	"fmt"
+	"math"
+	"math/rand/v2"
+	"test/custom/asm/asm"
+	"unsafe"
+)
+
+func main() {
+
+	a, b := make([]int16, 16), make([]int16, 16)
+	for i := 0; i < len(a); i++ {
+		a[i], b[i] = int16(rand.N(math.MaxUint8)), int16(rand.N(math.MaxUint8))
+	}
+
+	// 调用汇编实现的 AVX2 函数
+	result := asm.Avx2_ssd_int16(a, b)
+
+	// 打印部分结果
+	fmt.Println("Result (first 10 values):", result)
+	sum := make([]int32, 8)
+	C.avx2_ssd_int16((*C.int16_t)(unsafe.Pointer(&a[0])), (*C.int16_t)(unsafe.Pointer(&b[0])), (*C.int32_t)(unsafe.Pointer(&sum[0])))
+	fmt.Println("Result (first 10 values):", sum)
+}
