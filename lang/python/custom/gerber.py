c17e880f11b6e6cdc04f5cf9c6cb15b68e913a85
贾一饼 <lby.i@qq.com>
2024-11-27 12:22:35 +0800

    update

--- lang/python/custom/gerber.py
+++ lang/python/custom/gerber.py
@@ -1,4 +1,4 @@
 
 from pygerber.gerberx3.api.v2 import GerberFile
-
-GerberFile.from_file(r"XXX").parse().render_raster("output.jpg")
+# 有问题,和gerbev不完全一次
+GerberFile.from_file(r"XXX").parse().render_raster("output.png")
