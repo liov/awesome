d8d75c6c563ef2e51e5ee6ae51bd0317822ce4b9
贾一饼 <lby.i@qq.com>
2025-01-07 09:50:15 +0800

    update

--- env/k8s/tools/data/db/postgres/postgres.sh
+++ env/k8s/tools/data/db/postgres/postgres.sh
@@ -1,15 +1,15 @@
 pg_dumpall -U postgres -p 5432 > bak.sql
 psql -U postgres -f bak.sql
 
-kubectl exec pod_name  -n namespace -- pg_dumpall -U postgres -p 5432 > bak.sql
-kubectl exec pod_name  -n namespace -- pg_dump -U postgres -p 5432 -d test > bak.sql
+kubectl exec pod_name  -n tools -- pg_dumpall -U postgres -p 5432 > bak.sql
+kubectl exec pod_name  -n tools -- pg_dump -U postgres -p 5432 -d test > bak.sql
 
 # 待测试
-kubectl exec pod_name  -n namespace -- psql -U postgres < bak.sql
+kubectl exec pod_name  -n tools -- psql -U postgres < bak.sql
 --inserts #insert语句导出
 docker exec postgres-old pg_dumpall -U postgres | docker exec -i postgres-new psql -U postgres
 
-kubectl exec -it postgres-f9b466ff-lrv2z  -n tools --  psql -U postgres;
+kubectl exec -it pod_name  -n tools --  psql -U postgres;
 
 set time zone "Asia/Shanghai";
 SET TIMEZONE='Asia/Shanghai';
