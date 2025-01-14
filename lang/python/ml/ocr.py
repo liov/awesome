01e38f49827c215b3ed906e5bd2a944903a5c9dc
liov <lby.i@qq.com>
2025-01-14 22:34:58 +0800

    ocr

--- lang/python/ml/ocr.py
+++ lang/python/ml/ocr.py
@@ -1,6 +1,7 @@
 import easyocr
 import cv2
-reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
 
-image = cv2.imread(dir, cv2.IMREAD_GARY)
+reader = easyocr.Reader(['ch_sim','en']) # this needs to run only once to load the model into memory
+image = cv2.imread('/home/jyb/Pictures/20250114182855.jpg', cv2.IMREAD_GRAYSCALE)
 result = reader.readtext(image)
+print(result)
